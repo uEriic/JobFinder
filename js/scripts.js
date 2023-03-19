02f904e19353068e2523906c2a18a72791e4abcf
uEriic <ueriic0@gmail.com>
2023-03-18 20:20:10 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+	var btnMenuMobile = $('.btn-menumobile');
+	$('.btn-menumobile').on('click', function() {
+		$('.nav-container ul').toggleClass('open');
+	});
+});
